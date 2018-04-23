da7b0ba5aaf1e4b41fb24a59248636af53314aa7
Sam <sammyliebman@gmail.com>
2018-04-23 15:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  //if string is already in lowercase print 'i cant hear you'
   if (string.toLowerCase() === string) return "I can't hear you!"
+  //if string is already in uppercase return "YES INDEED"
   else if (string.toUpperCase() === string) return "YES INDEED!"
+  
   else if (string === "I love you, Grandma.") return "I love you, too."
 }
